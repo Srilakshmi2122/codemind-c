7030a5bd19073087d33eaf764d7aaddb71343390
Srilakshmi2122 <119720694+Srilakshmi2122@users.noreply.github.com>
2022-12-05 22:56:15 +0530

    #include<stdio.h> int main() {     int e,m,p,c,cp;     scanf("%d%d%d%d%d",&e,&m,&p,&c,&cp);     if(e>34 && m>34 && p>34 && c>34 && cp>34)     {         printf("PASSED");     }     else     {         printf("FAILED");     } }

---
+++ Passed_or_Failed.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int e,m,p,c,cp;
+    scanf("%d%d%d%d%d",&e,&m,&p,&c,&cp);
+    if(e>34 && m>34 && p>34 && c>34 && cp>34)
+    {
+        printf("PASSED");
+    }
+    else
+    {
+        printf("FAILED");
+    }
+}
